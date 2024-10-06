7cb19df940f5229dd4f263f1e6c87121aee75116
John Matthews <jwmatthews@gmail.com>
2024-10-06 11:58:45 -0400

    Setting up scripts to run kantra

---
+++ data/springboot/3/samples/fetch.sh
@@ -0,0 +1,8 @@
+git clone git@github.com:jwmatthews/tut-rest.git
+
+# We want to fetch the older SpringBoot 2.x version
+# https://github.com/spring-guides/tut-rest/tree/50cf9c74bfc244b23f707ab16af8b4ef31136b3c
+cd tut-rest
+git checkout 50cf9c74bfc244b23f707ab16af8b4ef31136b3c
+cd ..
+
